d9ae5359f77bb41d7342dce5955934de69517f2c
ggriffi <ggriffi@gmail.com>
2026-01-30 17:45:06 -0600

    updated from:

--- functions/api/lead.js
+++ functions/api/lead.js
@@ -80,7 +80,7 @@ UA: ${lead.ua}
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                from: env.LEAD_FROM_EMAIL || "contact@anchorpoint-it.com",
+                from: "AnchorPoint IT <onboarding@resend.dev>",
                 to: [env.LEAD_TO_EMAIL],
                 subject,
                 text,
